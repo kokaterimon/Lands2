a88a01c78cd1a9ddb6a3749ddedc6232e564c3a3
kokaterimon <36601241+kokaterimon@users.noreply.github.com>
2018-02-22 09:36:51 -0500

    Login View Model

---
+++ Lands2/Lands2/ViewModels/LoginViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace Lands2.ViewModels
+{
+    using System.Windows.Input;
+    class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        { get;
+          set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+        #region Commands
+        public ICommand LoginCommand
+        {
+          get;
+          set;
+        }
+        #endregion
+    }
+}
--- Lands2/Lands2/ViewModels/MainViewModel.cs
+++ Lands2/Lands2/ViewModels/MainViewModel.cs
@@ -6,5 +6,18 @@ namespace Lands2.ViewModels
 {
     class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
