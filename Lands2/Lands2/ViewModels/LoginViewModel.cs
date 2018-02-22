87cd7fe39045e9798efec41a10579aced7324a5c
kokaterimon <36601241+kokaterimon@users.noreply.github.com>
2018-02-22 13:41:43 -0500

    Base View Model

--- Lands2/Lands2/ViewModels/LoginViewModel.cs
+++ Lands2/Lands2/ViewModels/LoginViewModel.cs
@@ -1,40 +1,141 @@
 ﻿namespace Lands2.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
     using System.Windows.Input;
-    class LoginViewModel
+    using Xamarin.Forms;
+
+    class LoginViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
         #region Properties
         public string Email
-        { get;
-          set;
+        {
+            get;
+            set;
         }
         public string Password
         {
-            get;
-            set;
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if(this.password != value) //significa que el valor de la propiedad cambió
+                {
+                    this.password = value;
+                    //después de que la propiedad cambie hay que refrescar el atributo
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.Password)));
+                }
+            }
         }
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isRunning != value) //significa que el valor de la propiedad cambió
+                {
+                    this.isRunning = value;
+                    //después de que la propiedad cambie hay que refrescar el atributo
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+            }
         }
         public bool IsRemembered
         {
             get;
             set;
         }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                if (this.isEnabled != value) //significa que el valor de la propiedad cambió
+                {
+                    this.isEnabled = value;
+                    //después de que la propiedad cambie hay que refrescar el atributo
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsEnabled)));
+                }
+            }
+        }
         #endregion
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
         #region Commands
         public ICommand LoginCommand
         {
-          get;
-          set;
+            get
+            {
+                return new RelayCommand(Login);
+            }
+            //set; le quitamos esta propiedad ya que este comando es una propiedad de solo lectura.
+        }
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an password",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "jzuluaga55@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or Password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert(
+                    "OK",
+                    "yeah!!!",
+                    "Accept");
         }
         #endregion
     }
