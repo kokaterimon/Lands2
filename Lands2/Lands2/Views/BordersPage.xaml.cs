0368ebcbd77cd7b815ce5d5049f09b0dc003934e
kokaterimon <36601241+kokaterimon@users.noreply.github.com>
2018-03-03 04:44:11 -0500

    TerminadoBorders

---
+++ Lands2/Lands2/Views/BordersPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Lands2.Views
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BordersPage : ContentPage
+	{
+		public BordersPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
