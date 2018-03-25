112867a0b5844c173dcf4d6ff492f28f4ec16dd4
kokaterimon <36601241+kokaterimon@users.noreply.github.com>
2018-03-25 11:07:16 -0500

    persistensy realized

--- Lands2/Lands2/ViewModels/MenuItemViewModel.cs
+++ Lands2/Lands2/ViewModels/MenuItemViewModel.cs
@@ -46,7 +46,8 @@
                 var mainViewModel = MainViewModel.GetInstance();
                 mainViewModel.Token = string.Empty;
                 mainViewModel.TokenType = string.Empty;
-                Application.Current.MainPage = new LoginPage();
+                Application.Current.MainPage = new NavigationPage(
+                    new LoginPage());
             }
         }
         #endregion
